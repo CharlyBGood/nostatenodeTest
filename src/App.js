de838b7f7a1eeb10d1a321010ae81a3484fb35b0
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-07-23 18:38:53 -0300

    add components to login and logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
+import {useAuth0} from '@auth0/auth0-react';
 
 function App() {
 
+  const {isAuthenticated, isLoading} = useAuth0();
+
+  if (isLoading) return <h1>Loading........</h1>
   return (
     <div className="App">
       <h1>
         NoStateNode
       </h1>
-      <LoginButton />
+      {
+        isAuthenticated ? <LogoutButton /> : <LoginButton />
+      }
+      <Profile />
     </div>
   );
 }
