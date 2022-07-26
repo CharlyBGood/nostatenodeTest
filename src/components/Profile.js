30fdaba9eb52eb967c20ae294a9000aa124200a9
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-07-26 16:54:06 -0300

    change welcome section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
         <div>      
         <img className="header-img" src={user.picture} alt={user.name}/>
         <h2>Hola, {user.name}, qué lindo es que estés aquí!</h2>
-        <p>Iniciaste sesión con este correo: {user.email}</p>
+        {/* <p>Iniciaste sesión con este correo: {user.email}</p> */}
 
         {/* <JSONPretty className="json-div" data={user} /> */}
 
