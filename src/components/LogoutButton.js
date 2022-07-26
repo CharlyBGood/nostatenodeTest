7193002d5afef73a0f591a376404e10c6ada7222
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-07-26 01:49:27 -0300

    fix logout

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import {useAuth0} from '@auth0/auth0-react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-    const {logout} = useAuth0();
+  const { logout } = useAuth0();
 
   return (
-    <button className='log-button' onClick={() => logout()}>
-        Logout
+    <button className="log-button" onClick={() => logout({returnTo: window.location.origin})}>
+      Logout
     </button>
-  )
-}
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
