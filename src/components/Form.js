9907732d41119c96ffdce7a1a2d948af5f54624c
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-08-03 20:32:14 -0300

    add table

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "../stylesheets/Form.css";
 
 function Form() {
+    const [newLinkTitle, setNewLinkTitle] = useState() 
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        localStorage.setItem("hello", "yeah");
+    }
   return (
     <div className="main-container">
-      <div className="form-container">
+      <form onSubmit={handleSubmit} className="form-container">
         <label>Descriptive Title</label>
-        <input placeholder="Título" itemType="text" className="form-title" />
+        <input placeholder="Título" 
+        type="text" 
+        className="form-title" 
+        onChange={(e)=> setNewLinkTitle(e.target.value)} />
         <label>Link URL</label>
         <input placeholder="URL" itemType="url" className="form-content" />
-        <button className="form-btn" onClick={()=> alert('yeah cabrón!')}>Add</button>
-      </div>
+        <button className="form-btn" onClick={()=> alert(newLinkTitle)}>Add</button>
+      </form>
     </div>
   );
 }
